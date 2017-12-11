8c70413329e478221d60b859e33a135103d44c8b
Timothy Meixell <misternu@gmail.com>
2017-12-11 11:48:39 -0600

    Posts with index page and materialize

---
+++ lib/phoenix_blog/post.ex
@@ -0,0 +1,20 @@
+defmodule PhoenixBlog.Post do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias PhoenixBlog.Post
+
+
+  schema "posts" do
+    field :body, :string
+    field :title, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%Post{} = post, attrs) do
+    post
+    |> cast(attrs, [:title, :body])
+    |> validate_required([:title, :body])
+  end
+end
--- lib/phoenix_blog_web/controllers/page_controller.ex
+++ lib/phoenix_blog_web/controllers/page_controller.ex
@@ -1,7 +1,10 @@
 defmodule PhoenixBlogWeb.PageController do
   use PhoenixBlogWeb, :controller
 
+  alias PhoenixBlog.Repo
+  alias PhoenixBlog.Post
+
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", posts: Repo.all(Post)
   end
 end
